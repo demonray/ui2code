5ae9850c69328eb7a4d07fced76f966a2b990a39
pikayang <pikayang@webank.com>
2023-11-09 16:48:43 +0800

    feat: 新增进度条element-plus代码生成

--- webview-ui/src/components/generator/target/element-plus/html.ts
+++ webview-ui/src/components/generator/target/element-plus/html.ts
@@ -74,6 +74,18 @@ const tags: TagTemplate = {
     if (child) child = `\n${child}\n`; // 换行
     return `<${tag} ${typeStr} ${icon} ${round} ${size} ${plain} ${disabled} ${circle}>${child}</${tag}>`;
   },
+  "el-progress": (el: ComponentItemJson) => {
+    debugger
+    const { tag, type } = attrBuilder(el);
+    const typeStr = type ? `type="${type}"` : "";
+    const status = (el.status && el.status !== '') ? `status="${el.status}"` : "";
+    const percentage = `percentage="${el.percentage || 0}"`;
+    const strokeWidth = (el.strokeWidth && el.strokeWidth !== '') ? `stroke-width="${el.strokeWidth}"` : "";
+    let child = buildElButtonChild(el);
+
+    if (child) child = `\n${child}\n`; // 换行
+    return `<${tag} ${typeStr} ${status} ${percentage} ${strokeWidth}>${child}</${tag}>`;
+  },
   "el-input": (el: ComponentItemJson) => {
     const { tag, disabled, vModel, clearable, placeholder, width, type } = attrBuilder(el);
     const maxlength = el.maxlength ? `:maxlength="${el.maxlength}"` : "";
@@ -523,7 +535,8 @@ function getUsedComp(html: string) {
     "el-tab-pane",
     "el-menu",
     "el-sub-menu",
-    "el-menu-item"
+    "el-menu-item",
+    "el-progress",
   ]
     .filter((item) => html.indexOf(item) > -1)
     .map((it) => {
